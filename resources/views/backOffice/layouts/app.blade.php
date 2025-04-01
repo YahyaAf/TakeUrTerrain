05888127005fb4dcdd6d8e01301a209794cc75ca
Yahya afadisse <yahyaafadisse92@gmail.com>
2025-04-01 22:33:15 +0000

    fixer affichage localisation dans map pour le terrain

--- resources/views/backOffice/layouts/app.blade.php
+++ resources/views/backOffice/layouts/app.blade.php
@@ -8,9 +8,16 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.9.1/chart.min.js"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
+
     <style>
         body {
             font-family: 'Inter', sans-serif;
+        }
+
+        #map {
+            width: 100%;
+            height: 500px;  
         }
     </style>
 </head>
