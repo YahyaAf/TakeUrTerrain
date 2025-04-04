8dc555a2fb9ee835c2c4310a35af19a71ad27c2b
Yahya afadisse <yahyaafadisse92@gmail.com>
2025-04-04 19:39:32 +0000

    now en va travailler sur contact

---
+++ app/Mail/ContactMessage.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class ContactMessage extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $name;
+    public $email;
+    public $message;
+
+    /**
+     * Create a new message instance.
+     *
+     * @param $name
+     * @param $email
+     * @param $message
+     */
+    public function __construct($name, $email, $message)
+    {
+        $this->name = $name;
+        $this->email = $email;
+        $this->message = $message;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject('Nouveau message de contact')
+                    ->from($this->email)
+                    ->to('yahyaclarke0@gmail.com')
+                    ->view('emails.contact');
+    }
+}
