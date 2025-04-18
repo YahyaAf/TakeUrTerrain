672af904a431bb65cf4099a30b31f46c911273f3
Yahya afadisse <yahyaafadisse92@gmail.com>
2025-04-18 23:53:27 +0100

    fixer les permission pour crud permission also

--- resources/views/backOffice/layouts/sidebar.blade.php
+++ resources/views/backOffice/layouts/sidebar.blade.php
@@ -118,12 +118,14 @@
             <span>Roles</span>
         </a>
         
+        @permission('view-permission')
         <a href="{{ route('permissions.index') }}" class="flex items-center px-4 py-2.5 mb-1 rounded-lg text-gray-600 hover:bg-gray-100 transition-colors duration-200">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-3 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 11V7a4 4 0 118 0m-4 8v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2z" />
             </svg>
             <span>Permissions</span>
         </a>
+        @endpermission
     </div>
 </div>
 
