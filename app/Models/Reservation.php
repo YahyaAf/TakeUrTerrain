5b13c845c53e5dcdadceb11e6d6cb10ba931aa0a
Yahya afadisse <yahyaafadisse92@gmail.com>
2025-04-29 16:11:31 +0100

    ajouter relation de payment dans model Reservation

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -34,4 +34,9 @@ class Reservation extends Model
         return $this->hasOne(Payment::class);
     }
 
+    public function ticket()
+    {
+        return $this->hasOne(Ticket::class);
+    }
+
 }
